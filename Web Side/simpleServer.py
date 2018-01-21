55fe03271205c1eea365cc9164e3823baf934721
Aivant Goyal <aivantg_developer@yahoo.com>
2018-01-21 08:53:11 -0800

    Added Sigma Stuff

--- Web Side/simpleServer.py
+++ Web Side/simpleServer.py
@@ -7,5 +7,5 @@ Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print("serving at port", PORT)
 httpd.serve_forever()
